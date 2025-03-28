9a164cbe470f517961b71d9f698a94532803bb5b
Patrick <po44243@gmail.com>
2025-03-28 10:50:12 -0300

    corrigido alguns problemas de conflito de estilização, agora o body está sendo estilizado corretamente

--- melodia-magica/src/main.jsx
+++ melodia-magica/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+
 import Home from './routes/Home';
 import Login_page from './routes/Login_page';
 import Quiz from './routes/game_page';
