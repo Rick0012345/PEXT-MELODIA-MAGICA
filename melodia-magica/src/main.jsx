51352574d609cef8f4dcd4fe5e5cf19ce582b84a
Patrick <po44243@gmail.com>
2025-03-30 13:45:28 -0300

    save

--- melodia-magica/src/main.jsx
+++ melodia-magica/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
 import Home from './routes/Home';
 import Login_page from './routes/Login_page';
 import Quiz from './routes/game_page';
