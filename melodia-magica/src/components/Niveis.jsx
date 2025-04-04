3bf7008188cc69a53e3004dadf5476f9662163e3
Patrick <po44243@gmail.com>
2025-04-04 19:04:22 -0300

    x

--- melodia-magica/src/components/Niveis.jsx
+++ melodia-magica/src/components/Niveis.jsx
@@ -7,7 +7,6 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="container">
 
             <div className="parent">
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
@@ -20,7 +19,6 @@ const Home = () => {
                     </div>
                 ))}
             </div>
-        </div>
     );
 };
 
